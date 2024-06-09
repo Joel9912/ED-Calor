417b22bad3e3ff5fc7538d2b59c0bb67442a99a4
Joel9912 <joelcamcor9912@gmail.com>
2024-06-09 13:43:45 -0600

    Create Calor.py

---
+++ Calor.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+
+def condini(x,y):
+    z = np.ones_like(x)
+    borde = (x < 0.15) | (x > 1.9) | (y < 0.15) | (y > 1.9)
+    anillo = (x > 0.35) & (x < 1.7) & (y > 0.35) & (y < 1.7)
+    centro = (x > 0.5) & (x < 1.55) & (y > 0.5) & (y < 1.55)
+    z[borde] = 0
+    z[anillo] = 2
+    z[centro] = 3
+    return z
+
+x = np.linspace(0, 2, 41)
+y = np.linspace(0, 2, 41)
+X,Y = np.meshgrid(x,y)
+Z = condini(X,Y)
+
+fig2 = plt.figure()
+ax2 = fig2.subplots()
+
+c = ax2.pcolor(X, Y, Z,edgecolors='k', linewidths=0.2, cmap=cm.jet, shading='auto')
+
+ax2.set_xlabel("x", fontsize=15)
+ax2.set_ylabel("y", fontsize=15)
+fig2.colorbar(c)
+plt.ylim(-0.025,2.025)
+plt.xlim(-0.025,2.025)
+plt.show()
