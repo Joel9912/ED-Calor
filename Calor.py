628225f9cda60b6b716698b84ee799abc67449dd
Joel9912 <joelcamcor9912@gmail.com>
2024-06-21 14:20:59 -0600

    Calor 1.1

--- Calor.py
+++ Calor.py
@@ -3,25 +3,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm
 
-pm = 500  # calor bajo
-pn = 10000  # calor máximo
-diffusion_const = 1.2e-5  # difusividad térmica para el acero en m^2/s
-delta_t = 0.5  # paso de tiempo, puede necesitar ajuste dependiendo de la estabilidad numérica
-delta_x = delta_y = 2 / 160  # tamaño de la celda espacial
+pm = 50  # calor bajo
+pn = 300  # calor máximo
+diffusion_const = 1.0e-4  # difusividad térmica para el acero en m^2/s
+delta_t = 0.2  # paso de tiempo
+delta_x = delta_y = 1 / 100  # tamaño de la celda espacial
 
 def condini(x, y):
     z = np.zeros_like(x)  # iniciamos con calor 0
-    anillo = np.sqrt((x - 1) ** 2 + (y - 1) ** 2) < 0.9  # generamos un anillo de calor
-    centro1 = np.sqrt((x - 1) ** 2 + (y - 1) ** 2) < 0.15  # la zona central sea caliente
-    centro2 = np.sqrt((x - 1) ** 2 + (y - 1) ** 2) < 0.12  # varios anillos para hacer una transición
-    centro3 = np.sqrt((x - 1) ** 2 + (y - 1) ** 2) < 0.09
-    centro4 = np.sqrt((x - 1) ** 2 + (y - 1) ** 2) < 0.06
 
-    z[anillo] = pm  # calor del anillo
-    z[centro1] = pn / 4  # hacemos la transición de calor en el punto central
-    z[centro2] = pn / 2
-    z[centro3] = pn / 1.5
-    z[centro4] = pn
+    # Líneas contiguas de calor bajo
+    z[:, -1] = 0  # borde derecho
+
+    # Líneas de calor en los bordes
+    z[:, 0] = pn  # borde izquierdo
+    z[0, :] = pn  # borde inferior
+    z[-1, :] = pn  # borde superior
 
     return z
 
@@ -29,72 +26,66 @@ def act_estado(Z):
     nx, ny = Z.shape
     Z_nuevo = np.copy(Z)  # se realiza una copia para poder modificar el estado
 
-    for i in range(nx):
-        for j in range(ny):
-            sum_diff = 0
-            num_neighbors = 0
+    for i in range(1, nx - 1):
+        for j in range(1, ny - 1):
+            sum_diff = (
+                Z[i-1, j] + Z[i+1, j] + Z[i, j-1] + Z[i, j+1]
+            )
+            Z_nuevo[i, j] = Z[i, j] + delta_t * diffusion_const * (
+                (sum_diff - 4 * Z[i, j]) / (delta_x**2)
+            )
 
-            if i > 0:
-                sum_diff += Z[i-1, j]  # Arriba
-                num_neighbors += 1
-            if i < nx-1:
-                sum_diff += Z[i+1, j]  # Abajo
-                num_neighbors += 1
-            if j > 0:
-                sum_diff += Z[i, j-1]  # Izquierda
-                num_neighbors += 1
-            if j < ny-1:
-                sum_diff += Z[i, j+1]  # Derecha
-                num_neighbors += 1
+    # Líneas contiguas de calor bajo
+    Z_nuevo[:, -1] = 0  # borde derecho
 
-            # Actualizar el estado utilizando la suma de diferencias y el número de vecinos
-            if num_neighbors > 0:
-                Z_nuevo[i, j] = Z[i, j] + delta_t * diffusion_const * (
-                    (sum_diff - num_neighbors * Z[i, j]) / (delta_x**2)
-                )
+    # Líneas de calor en los bordes
+    Z_nuevo[:, 0] = pn  # borde izquierdo
+    Z_nuevo[0, :] = pn  # borde inferior
+    Z_nuevo[-1, :] = pn  # borde superior
 
     return Z_nuevo
 
 # Generación de la malla y sus condiciones iniciales
 a = b = 2
 
-x = np.linspace(0, a, 81)
-y = np.linspace(0, b, 81)
+x = np.linspace(0, a, 101)
+y = np.linspace(0, b, 101)
 X, Y = np.meshgrid(x, y)
 Z = condini(X, Y)
 
-# Guardamos las actualizaciones en una carpeta personalizada
+# Crear directorio de salida si no existe
 output_dir = 'D:\\Usuario\\Documentos\\CursosUCR\\Física\\Física-Computacional\\Proyecto'
 os.makedirs(output_dir, exist_ok=True)
 
 # Primera imagen con las condiciones iniciales
-fig = plt.figure()
-ax = fig.subplots()
+fig = plt.figure(figsize=(8, 6), dpi=400)
+ax = fig.add_subplot(111)
 
-color_plot = ax.pcolor(X, Y, Z, edgecolors='k', linewidths=0.2, cmap=cm.jet, shading='auto', vmin=0, vmax=pn)
+color_plot = ax.pcolor(X, Y, Z, edgecolors='k', linewidths=0.1, cmap=cm.jet, shading='auto', vmin=0, vmax=pn)
 ax.set_xlabel("x", fontsize=15)
 ax.set_ylabel("y", fontsize=15)
 fig.colorbar(color_plot)
-plt.ylim(-0.0125, 2.0125)
-plt.xlim(-0.0125, 2.0125)
+plt.ylim(-0.01, 2.01)
+plt.xlim(-0.01, 2.01)
 
-plt.savefig(os.path.join(output_dir, f'iteracion_{0}.png'))
+plt.savefig(os.path.join(output_dir, f'iteracion_{0}.png'), bbox_inches='tight', pad_inches=0.1)
 plt.close(fig)
 
-# Iteramos para calcular las nuevas temperaturas
+# Iterar para calcular las nuevas temperaturas
 n = 200
 for k in range(n):
-    Z = act_estado(Z)
+    Z[1:-1, 1:-1] = act_estado(Z[1:-1, 1:-1])  # Actualizar el estado sin modificar los bordes
 
-    fig = plt.figure()
-    ax = fig.subplots()
+    # Guardar la figura de cada iteración
+    fig = plt.figure(figsize=(8, 6), dpi=400)
+    ax = fig.add_subplot(111)
 
-    color_plot = ax.pcolor(X, Y, Z, edgecolors='k', linewidths=0.2, cmap=cm.jet, shading='auto', vmin=0, vmax=pn)
+    color_plot = ax.pcolor(X, Y, Z, edgecolors='k', linewidths=0.1, cmap=cm.jet, shading='auto', vmin=0, vmax=pn)
     ax.set_xlabel("x", fontsize=15)
     ax.set_ylabel("y", fontsize=15)
     fig.colorbar(color_plot)
-    plt.ylim(-0.0125, 2.0125)
-    plt.xlim(-0.0125, 2.0125)
+    plt.ylim(-0.01, 2.01)
+    plt.xlim(-0.01, 2.01)
 
-    plt.savefig(os.path.join(output_dir, f'iteracion_{k+1}.png'))
+    plt.savefig(os.path.join(output_dir, f'iteracion_{k+1}.png'), bbox_inches='tight', pad_inches=0.1)
     plt.close(fig)
